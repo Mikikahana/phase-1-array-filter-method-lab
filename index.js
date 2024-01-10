5eb8cf3440c140dc792679d7d6d74cd9e6a5c61c
Mikikahana <kmichoel25@gmail.com>
2024-01-09 19:25:30 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (name, horn) {
+return name.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === horn.toLowerCase()
+);
+}
+
+function fuzzyMatch (name, prefix) {
+    return name.filter(
+       (possibleMatch) => 
+       possibleMatch.toLowerCase().indexOf(prefix.toLowerCase()) === 0
+   );
+}
+function matchName (name, prefix) {
+    return name.filter((record) => record.name === prefix);
+    }
